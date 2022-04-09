b5ef08ebf7b2c7bd1a628629f396c17253345de5
Troy C Graves <sftroychance@gmail.com>
2022-04-08 20:13:00 -0400

    update exercises

---
+++ OOP_exercises/basics_classes+objects_2/counting_cats.rb
@@ -0,0 +1,25 @@
+=begin
+
+Using the following code, create a class named Cat that tracks the number of times a new Cat object is instantiated. The total number of Cat instances should be printed when ::total is invoked.
+
+Expected output:
+2
+
+=end
+
+class Cat
+  @@number_of_cats = 0
+
+  def self.total
+    puts @@number_of_cats
+  end
+
+  def initialize
+    @@number_of_cats += 1
+  end
+end
+
+kitty1 = Cat.new
+kitty2 = Cat.new
+
+Cat.total
---
+++ OOP_exercises/basics_classes+objects_2/public_secret.rb
@@ -0,0 +1,21 @@
+=begin
+Using the following code, create a class named Person with an instance variable
+named @secret. Use a setter method to add a value to @secret, then use a getter
+method to print @secret.
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+puts person1.secret
+
+Expected output:
+
+Shh.. this is a secret!
+=end
+
+class Person
+  attr_accessor :secret
+end
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+puts person1.secret
